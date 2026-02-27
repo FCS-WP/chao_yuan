d66fd38ea5f83af148b96d1423b58656a1053aaf
Lucass1611 <chuong.nguyenquoc@floatingcube.com>
2026-02-27 15:11:42 +0700

    Init code

--- src/wp-content/themes/zippy-elementor-child/assets/js/app.js
+++ src/wp-content/themes/zippy-elementor-child/assets/js/app.js
@@ -1,3 +1,24 @@
+import "../slick/slick.min";
 $(document).ready(function ($) {
   
+});
+
+$("#hp-our-reviews").slick({
+  autoplay: true,
+  autoplaySpeed: 1200,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows:false
+      },
+    },
+  ],
 });
